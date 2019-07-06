2df3d22f322a9cbf380e8277812c9922132c2ebd
Joshua Crum <joshua.crum@gmail.com>
2019-07-06 20:19:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,46 +13,60 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get "/signup" do
-		erb :signup
-	end
+    erb :signup
+  end
 
-	post "/signup" do
-		#your code here!
-	end
+  post "/signup" do
+		user = User.new(:username => params[:username], :password => params[:password])
+  		if user.save
+    	redirect "/login"
+  	else
+    	redirect "/failure"
+  	end
+  end
 
-	get "/login" do
-		erb :login
-	end
+  get '/account' do
+    @user = User.find(session[:user_id])
+    erb :account
+  end
 
-	post "/login" do
-		#your code here!
-	end
+
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+		user = User.find_by(:username => params[:username])
+
+  	if user && user.authenticate(params[:password])
+    	session[:user_id] = user.id
+    	redirect "/success"
+  	else
+    	redirect "/failure"
+  	end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
 
 	get "/success" do
-		if logged_in?
-			erb :success
-		else
-			redirect "/login"
-		end
+		erb :success
 	end
 
-	get "/failure" do
-		erb :failure
-	end
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
 
-	get "/logout" do
-		session.clear
-		redirect "/"
-	end
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
 
-	helpers do
-		def logged_in?
-			!!session[:user_id]
-		end
-
-		def current_user
-			User.find(session[:user_id])
-		end
-	end
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
 
 end
